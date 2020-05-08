1e348cfe5c5b3879d37605a1ba9fd55f7bc58ad0
Muelle <janni.mueller@yahoo.de>
2020-05-08 12:43:36 +0200

    angepasst

--- components/catalog/ShopCard.vue
+++ components/catalog/ShopCard.vue
@@ -2,25 +2,29 @@
   <div class="">
     <!--Image of Card -->
     <img class="w-full object-cover sm:object-center rounded-lg"
-         :src="product.imageUrl"
-         :alt="product.title">
+         :src="product.details.imageUrl"
+         :alt="product.details.title">
     <!--Textpill of Card -->
     <div class="px-1 py-6">
       <div class="relative px-6 py-4 -mt-12 bg-white shadow-lg rounded-lg">
         <div class="text-gray-900 antialiased">
-          <div class="text-gray-600 text-xs uppercase font-semibold tracking-wide truncate">{{product.author}}</div>
+          <span v-for="author in product.authors">
+          <div class="text-gray-600 text-xs uppercase font-semibold tracking-wide truncate">{{author.name}}</div>
+          </span>
           <div class="text-anthrazit font-bold text-md truncate">
-            {{product.title}}
+            {{product.details.title}}
           </div>
           <div class="border-b-2 border-gray-300 w-2/12 pl-1 py-1"></div>
           <p class="text-gray-700 sm:text-md md:text-sm truncate pt-2">
-            {{product.description}}
+            {{product.details.description}}
           </p>
           <p class="text-anthrazit text-md mt-1">
-            {{product.listPrice}} € &bullet;
+            {{product.details.listPrice}} € &bullet;
+            <span v-for="category in product.categories">
             <span class="inline-block bg-primary rounded-full px-3 py-1 text-sm font-semibold text-white mr-2">
-            {{product.category}}
+            {{category.title}}
           </span>
+              </span>
           </p>
         </div>
       </div>
